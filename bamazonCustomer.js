9e396821b9a78d174c0b6d7c6e9bdd15feedbd40
Logan Bertrand <loganwbertrand@gmail.com>
2019-05-15 14:17:17 -0700

    added inquirer and more code in js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 //Require Necessary Modules
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -24,7 +25,40 @@ connection.connect(function(err) {
 function start() {
   connection.query("SELECT item_id, product_name, price FROM products", function(err, res) {
     if (err) throw err;
-    console.log(res);
-    connection.end();
+    console.table(res);
+    askCustomer();
   });
+}
+
+function askCustomer(){
+    inquirer
+    .prompt([
+      {
+        name: "item",
+        type: "input",
+        message: "What is the id of the item you would like to purchase?"
+      },
+      {
+        name: "amount",
+        type: "input",
+        message: "How many of that item would you like to purchase?",
+        validate: function(value) {
+            if (isNaN(value) === false) {
+              return true;
+            }
+            return false;
+          }
+      },
+    ])
+    .then(function(answer) {
+        results();
+    })
+    
+}
+
+function results(){
+    //If there are not that many to purchase, return error
+
+    //Otherwise fulfill the order and deduct the amount bought from the inventory
+    //And return the total cost of the purchase
 }
